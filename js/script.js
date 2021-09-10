144798f541e244c7f74b6a396534642975d2176d
rasoirnoir <w.noris@protonmail.com>
2021-09-10 11:49:11 +0200

    section accordéon

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 function sectionClick(){
     let target = event.target;
-    console.log(target);
 
     while(target.nodeName  != "SECTION"){
         target = target.parentNode;
     }
 
+    console.log(target.querySelector(".visible"));
+    console.log(target.querySelector(".invisible"));
 
     let listeDeroulante = target.querySelector(".visible") || target.querySelector(".invisible");
 
-    if(listeDeroulante.className.indexOf("visible") > -1){
+    console.log("visible : " + listeDeroulante.className.indexOf("visible"));
+    console.log("invisible : " + listeDeroulante.className.indexOf("invisible"));
+
+    if(listeDeroulante.className.indexOf("visible") == 0){
         listeDeroulante.className = "invisible";
     }
     else{
         listeDeroulante.className = "visible";
     }
     
+    console.log("Liste déroulante : " + listeDeroulante + " classe : " + listeDeroulante.className);
 
 
 }
