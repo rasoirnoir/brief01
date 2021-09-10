9e20928ae545f7947be617e76b00caf934ba1b22
rasoirnoir <w.noris@protonmail.com>
2021-09-10 10:51:14 +0200

    avancée sur le javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 function sectionClick(){
-    alert("click");
+    let target = event.target;
+    console.log(target);
+
+    while(target.nodeName  != "SECTION"){
+        target = target.parentNode;
+    }
+
+
+    let listeDeroulante = target.querySelector(".visible") || target.querySelector(".invisible");
+
+    if(listeDeroulante.className.indexOf("visible") > -1){
+        listeDeroulante.className = "invisible";
+    }
+    else{
+        listeDeroulante.className = "visible";
+    }
+    
+
+
 }
